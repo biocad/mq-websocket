fd7b27b82535cb87b31dce3b277aa227b2bc5b6b
vks4git <vks_m@niuitmo.ru>
2018-04-27 10:22:34 +0300

    Lifted functions und kleine Ordnung

---
+++ src/System/MQ/Websocket/Atomic/Functions.hs
@@ -0,0 +1,103 @@
+module System.MQ.Websocket.Atomic.Functions
+  (
+    packConnection
+  , packConnectionWithSpec
+  , clientsCount
+  , clientsCountM
+  , clientExists
+  , clientExistsM
+  , addConnection
+  , addConnectionM
+  , removeConnection
+  , removeConnectionM
+  , allConnections
+  , allConnectionsM
+  , sharedClients
+  ) where
+
+import           Control.Concurrent.STM.TVar      (TVar, modifyTVar', newTVarIO,
+                                                   readTVarIO)
+import           Control.Monad.IO.Class           (MonadIO (..))
+import           Control.Monad.STM                (atomically)
+import           Data.List                        (union, (\\))
+import           Data.Map.Strict                  (empty, insertWith, member,
+                                                   size, toList, update)
+import qualified Network.WebSockets               as WS
+import           System.Clock                     (Clock (..), getTime,
+                                                   toNanoSecs)
+import           System.IO.Unsafe                 (unsafePerformIO)
+import           System.MQ.Websocket.Atomic.Types (ClientConnection, ClientId,
+                                                   Clients, Spec, Timestamp,
+                                                   WSConnection (..))
+
+-- | Shared between FromWS and FromMQ processes memory pointer with all connected clients and specs they have requested.
+-- unsafePerformIO is adviced here by the authors of Control.Concurrent
+--
+sharedClients :: TVar Clients
+sharedClients = unsafePerformIO . newTVarIO $ empty
+
+
+-- | Pack a WebSocket connection into a wrapper which is Eq and Ord instance
+-- and stores for each connection all specs that the client has requested.
+--
+packConnection :: MonadIO m => WS.Connection -> m WSConnection
+packConnection connection = packConnectionWithSpec connection []
+
+packConnectionWithSpec :: MonadIO m => WS.Connection -> [Spec] -> m WSConnection
+packConnectionWithSpec connection specs = WSConnection <$> getTimeNano <*> pure connection <*> pure specs
+
+-- | Pure and lifted versions of clients count check.
+-- Lifted version uses @sharedClients@ `TVar` to obtain all connected clients.
+--
+clientsCount :: Clients -> Int
+clientsCount = size
+
+clientsCountM :: MonadIO m => m Int
+clientsCountM = liftIO $ clientsCount <$> readTVarIO sharedClients
+
+
+-- | Pure and lifted versions of client existance check.
+-- Lifted version uses @sharedClients@ `TVar` to obtain all connected clients.
+--
+clientExists :: ClientId -> Clients -> Bool
+clientExists = member
+
+clientExistsM :: MonadIO m => ClientId -> m Bool
+clientExistsM clientId = liftIO $ clientExists clientId <$> readTVarIO sharedClients
+
+
+-- | Pure and lifted versions of connection addition.
+-- Lifted version uses @sharedClients@ `TVar` to obtain all connected clients.
+--
+addConnection :: ClientConnection -> Clients -> Clients
+addConnection (clientId, connection) = insertWith union clientId [connection]
+
+addConnectionM :: MonadIO m => ClientConnection -> m ()
+addConnectionM = liftIO . atomically . modifyTVar' sharedClients . addConnection
+
+
+-- | Pure and lifted versions of connection removal.
+-- Lifted version uses @sharedClients@ `TVar` to obtain all connected clients.
+--
+removeConnection :: ClientConnection -> Clients -> Clients
+removeConnection (userId, connection) = update searchAndRemove userId
+  where searchAndRemove connections = Just $ connections \\ [connection]
+
+removeConnectionM :: MonadIO m => ClientConnection -> m ()
+removeConnectionM = liftIO . atomically . modifyTVar' sharedClients . removeConnection
+
+
+-- | Pure and lifted versions of obtaining all WebSocket connections.
+-- Lifted version uses @sharedClients@ `TVar` to obtain all connected clients.
+--
+allConnections :: Clients -> [WS.Connection]
+allConnections = concatMap (fmap wsConnection) . fmap snd . toList
+
+allConnectionsM :: MonadIO m => m [WS.Connection]
+allConnectionsM = liftIO $ allConnections <$> readTVarIO sharedClients
+
+
+-- | Get current Epoch time in nanoseconds.
+--
+getTimeNano :: MonadIO m => m Timestamp
+getTimeNano = liftIO $ fromIntegral . toNanoSecs <$> getTime Realtime
--- src/System/MQ/Websocket/Atomic/Types.hs
+++ src/System/MQ/Websocket/Atomic/Types.hs
@@ -5,31 +5,17 @@ module System.MQ.Websocket.Atomic.Types
   , Clients
   , ClientConnection
   , WSConnection (..)
-  , packConnection
-  , packConnectionWithSpec
-  , clientsCount
-  , clientExists
-  , addConnection
-  , removeConnection
-  , allConnections
-  , sharedClients
+  , Timestamp
   ) where
 
-import           Control.Concurrent.STM.TVar (TVar, newTVarIO)
-import           Control.Monad.IO.Class      (MonadIO (..))
-import           Data.List                   (union, (\\))
-import           Data.Map.Strict             (Map, empty, insertWith, member,
-                                              size, toList, update)
-import           Data.Text                   (Text)
-import qualified Network.WebSockets          as WS
-import           System.Clock                (Clock (..), getTime, toNanoSecs)
-import           System.IO.Unsafe            (unsafePerformIO)
+import           Data.Map.Strict    (Map)
+import           Data.Text          (Text)
+import qualified Network.WebSockets as WS
+import           System.MQ.Protocol (Spec)
 
 type ClientId = Text
 
 type Timestamp = Int
-
-type Spec = String
 
 type Clients = Map ClientId [WSConnection]
 
@@ -49,32 +35,3 @@ instance Ord WSConnection where
   wsConn1 <= wsConn2 = wsTime wsConn1 <= wsTime wsConn2
 
 
--- | unsafePerformIO is adviced here by the authors of Control.Concurrent
---
-sharedClients :: TVar Clients
-sharedClients = unsafePerformIO . newTVarIO $ empty
-
-packConnection :: MonadIO m => WS.Connection -> m WSConnection
-packConnection connection = packConnectionWithSpec connection []
-
-packConnectionWithSpec :: MonadIO m => WS.Connection -> [Spec] -> m WSConnection
-packConnectionWithSpec connection specs = WSConnection <$> getTimeNano <*> pure connection <*> pure specs
-
-clientsCount :: Clients -> Int
-clientsCount = size
-
-clientExists :: ClientId -> Clients -> Bool
-clientExists = member
-
-addConnection :: ClientConnection -> Clients -> Clients
-addConnection (clientId, connection) = insertWith union clientId [connection]
-
-removeConnection :: ClientConnection -> Clients -> Clients
-removeConnection (userId, connection) = update searchAndRemove userId
-  where searchAndRemove connections = Just $ connections \\ [connection]
-
-allConnections :: Clients -> [WS.Connection]
-allConnections = concatMap (fmap wsConnection) . fmap snd . toList
-
-getTimeNano :: MonadIO m => m Timestamp
-getTimeNano = liftIO $ fromIntegral . toNanoSecs <$> getTime Realtime
