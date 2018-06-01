9f37405d2beca02c24fdc48bae732b3ac58fcc8b
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-06-01 14:18:00 +0300

    updation for the protocol is started

--- src/System/MQ/WebSocket/Atomic/Types.hs
+++ src/System/MQ/WebSocket/Atomic/Types.hs
@@ -29,7 +29,7 @@ import           Data.Text                     (Text)
 import           GHC.Generics                  (Generic)
 import qualified Network.WebSockets            as WS
 import           System.MQ.Protocol            (Dictionary (..), MessageTag,
-                                                Spec)
+                                                Spec, MessageType)
 
 -- | Name for the application
 websocketName :: String
@@ -66,16 +66,43 @@ type Specs = Map Spec [ClientConnection]
 
 -- | WebSocket connection wrapper type.
 -- Creation time is used as an identifier which helps to distinguish connections.
-data WSConnection = WSConnection { wsTime         :: Timestamp     -- ^ Connection creation time. Acts as identifier.
-                                 , wsConnection   :: WS.Connection -- ^ Connection itself
-                                 , wsAcceptedSpec :: [Spec]        -- ^ Specs which creator of the connection accepts.
-                                 }
+data WSConnection = WSConnection {-# UNPACK #-} !Timestamp     -- ^ Connection creation time. Acts as identifier.
+                                 {-# UNPACK #-} !ClientId      -- ^ Client identificator
+                                 {-# UNPACK #-} !WS.Connection -- ^ Connection itself
+                                 
 
 instance Eq WSConnection where
-  wsConn1 == wsConn2 = wsTime wsConn1 == wsTime wsConn2
+  (WSConnection t1 _ _) == (WSConnection t2 _ _) = t1 == t2
 
 instance Ord WSConnection where
-  wsConn1 <= wsConn2 = wsTime wsConn1 <= wsTime wsConn2
+  (WSConnection t1 _ _) <= (WSConnection t2 _ _) = t1 <= t2
+
+-- -- | 'MessageType' describes valid message types in Monique with additional .
+-- --
+-- data MessageType = Config | Result | Error | Data | AnyType
+--   deriving (Eq, Generic)
+
+-- instance Show MessageType where
+--   show Config  = "config"
+--   show Result  = "result"
+--   show Error   = "error"
+--   show Data    = "data"
+--   show AnyType = "*"
+
+-- instance Read MessageType where
+--   readsPrec _ "config" = [(Config, "")]
+--   readsPrec _ "result" = [(Result, "")]
+--   readsPrec _ "error"  = [(Error, "")]
+--   readsPrec _ "data"   = [(Data, "")]
+--   readsPrec _ "*"      = [(AnyType, "")]
+--   readsPrec _ _        = []
+
+-- | Subscription is 
+data Subscription = Subscription {-# UNPACK #-} Spec
+                                 {-# UNPACK #-} MessageType
+
+
+
 
 newtype ConnectionSetup = ConnectionSetup { specs :: [String] }
   deriving (Generic, Show)
---
+++ src/System/MQ/WebSocket/Protocol/Types.hs
@@ -0,0 +1,51 @@
+module System.MQ.WebSocket.Protocol.Types where
+
+import           Control.Monad                 ((>=>))
+import           Data.Aeson                    (FromJSON (..), ToJSON (..))
+import           Data.ByteString               (ByteString)
+import qualified Data.ByteString.Lazy          as BSL (ByteString)
+import           Data.Map.Strict               (Map, fromList, member, (!))
+import           Data.MessagePack.Types.Class  (MessagePack (..))
+import           Data.MessagePack.Types.Object (Object)
+import           Data.Text                     (Text)
+import           GHC.Generics                  (Generic)
+import qualified Network.WebSockets            as WS
+import           System.MQ.Protocol            (Dictionary (..), MessageTag,
+                                                Spec, MessageType)
+-- | Current timestamp in nanoseconds. Nanoseconds are used to decrease probability
+-- that different connections will have the same identifier.
+type Timestamp = Int
+
+-- | Describes which messages with @spec@ and @type@ are interested for the current connection.
+-- To get all @spec@s wildcard symbol ("*") is used.
+-- To get all @type@s the same symbol is used.
+-- 
+data Subscription = Subscription { subSpec :: Text -- ^ str; subscription specification
+                                 , subType :: Text -- ^ str; subscription type
+                                 }
+  deriving (Eq, Ord, Show)
+
+-- | 'WSMessage' represents data that sent via WebSocket connection to and from MoniQue system.
+--
+data WSMessage = WSMessage { wsTag     :: MessageTag -- ^ bin; tag in bytestring
+                           , wsMessage :: ByteString -- ^ bin; message content, packed in MessagePack
+                           }
+  deriving (Eq, Ord, Show)
+
+-- | Describes high level of websocket protocol.
+--
+data WSData = WSPing         Timestamp
+            | WSPong         Timestamp
+            | WSSubscribe    [Subscription]
+            | WSSubscribed   [Subscription]
+            | WSUnsubscribe  [Subscription]
+            | WSUnsubscribed [Subscription]
+            | WSPushToMQ     WSMessage
+            | WSPushedToMQ   WSMessage
+            | WSPushedFromMQ WSMessage
+  deriving (Eq, Ord, Show)
+
+-- | Tiny helpful class to marks messages with the command.
+--
+class CommandLike a where
+    command :: a -> Text
