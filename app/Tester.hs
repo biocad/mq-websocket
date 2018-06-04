3e530c9767143855c0d1aaa13e496681b828f59a
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-06-04 17:55:45 +0300

    works

--- app/Tester.hs
+++ app/Tester.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE ViewPatterns      #-}
 
 module Main where
 
 import           Control.Concurrent             (forkIO)
+import Control.Monad.IO.Class (liftIO)
 import           Control.Monad                  (forever)
 import           Data.Aeson                     (FromJSON (..), ToJSON (..))
 import           Data.Aeson.Picker              ((|--))
 import qualified Data.ByteString                as BS
 import           Data.CaseInsensitive           (CI (..))
+import           Data.MessagePack               (MessagePack (..))
+import           Data.Text                      as T (pack)
 import           GHC.Generics                   (Generic)
 import           Network.Socket                 (withSocketsDo)
 import qualified Network.WebSockets             as WS
@@ -17,12 +22,15 @@ import           System.BCD.Config              (getConfigText)
 import qualified System.MQ.Encoding.JSON        as JSON (pack, unpack)
 import qualified System.MQ.Encoding.MessagePack as MP (pack, unpack)
 import           System.MQ.Monad                (runMQMonad)
-import           System.MQ.Protocol             (MessageLike (..),
+import           System.MQ.Protocol             (Message (..), MessageLike (..),
                                                  MessageType (..), Props (..),
-                                                 createMessage, emptyHash,
+                                                 createMessageBS, emptyHash,
                                                  jsonEncoding, messageTag,
                                                  notExpires)
-import           System.MQ.WebSocket.Protocol   (WSData (..), WSMessage (..))
+import           System.MQ.WebSocket            (getTimeNano)
+import           System.MQ.WebSocket.Protocol   (CommandLike (..),
+                                                 Subscription (..), WSData (..),
+                                                 WSMessage (..))
 
 -- | 'CalculatorConfig' represents configuration data for calculator.
 --
@@ -53,7 +61,7 @@ cookie = [("Cookie", "id=0000-0000-0000-websocket-tester")]
 processer :: WS.ClientApp ()
 processer connection = do
     -- initialize with "example_calculator" spec
-    WS.sendTextData connection $ ("asdasd" :: BS.ByteString) --MP.pack $ WSPong 123 --  (JSON.pack $ ConnectionSetup ["example_calculator"])
+    -- WS.sendTextData connection $ ("asdasd" :: BS.ByteString) --MP.pack $ WSPong 123 --  (JSON.pack $ ConnectionSetup ["example_calculator"])
     putStrLn "Initialized..."
 
     _ <- forkIO $ forever $ listener connection
@@ -65,33 +73,57 @@ listener :: WS.ClientApp ()
 listener connection = forever $ do
     msg <- WS.receiveData connection :: IO BS.ByteString
     putStrLn "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    putStrLn "---------- FULL MESSAGE"
+    putStrLn "FULL MESSAGE:"
     print msg
 
-    if msg == "pong"
-    then putStrLn "---------- RECEIVED \"pong\"" >> putStrLn ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    else do
-      putStrLn "---------- FULL MESSAGE (HEX)"
-      putStrLn $ unwords $ map (`showHex` "") (BS.unpack msg)
+    putStrLn "\nFULL MESSAGE (HEX):"
+    putStrLn $ unwords $ map (`showHex` "") (BS.unpack msg)
 
-      case MP.unpack msg :: (Maybe WSMessage) of
+    case MP.unpack msg :: (Maybe WSData) of
+        Just msgUnpacked@(WSPushedFromMQ (WSMessage tag' msg')) -> do
+            putStrLn "\nCOMMAND"
+            print $ command msgUnpacked
+            putStrLn "\nTAG FROM MQ"
+            print tag'
+            putStrLn "\nMESSAGE FROM MQ (HEX)"
+            putStrLn $ unwords $ map (`showHex` "") (BS.unpack msg')
+            putStrLn "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
+
         Just msgUnpacked -> do
-            putStrLn "---------- TAG"
-            print $ wsTag msgUnpacked
-            putStrLn "---------- MESSAGE"
-            print $ wsMessage msgUnpacked
-            putStrLn "---------- MESSAGE (HEX)"
-            putStrLn $ unwords $ map (`showHex` "") (BS.unpack $ wsMessage msgUnpacked)
-            putStrLn ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+            putStrLn "\nCOMMAND"
+            print $ command msgUnpacked
+            -- putStrLn "\nDATA"
+            -- print $ wsMessage msgUnpacked
+            -- putStrLn "\nMESSAGE (HEX)"
+            -- putStrLn $ unwords $ map (`showHex` "") (BS.unpack $ wsMessage msgUnpacked)
+            putStrLn "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
 
         Nothing -> do
-            putStrLn "ERROR: could not unpack WSMessage"
-            putStrLn ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+            putStrLn "\nERROR: could not unpack WSMessage"
+            putStrLn "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
 
 speaker :: WS.ClientApp ()
 speaker connection = forever $ do
-    WS.sendTextData connection ("ping" :: BS.ByteString)
-    putStrLn "Enter first number: "
+    comm <- getLine
+    case words comm of
+        ["ping"] -> do
+            now <- getTimeNano
+            sendToServer $ WSPing now
+        ["pong"] -> do
+            now <- getTimeNano
+            sendToServer $ WSPing now
+        ["subscribe", spec', type'] -> do
+            sendToServer $ WSSubscribe [Subscription (T.pack spec') (T.pack type')]
+        ["push", spec', read -> type', encoding', path] -> do
+            dataBS <- liftIO $ BS.readFile path
+            msg@Message{..} <- runMQMonad $ createMessageBS emptyHash "0000-0000-0000-websocket-tester" notExpires spec' encoding' type' dataBS
+            let tag = messageTag msg
+            sendToServer . WSPushToMQ . WSMessage tag $ MP.pack msg
+        _        -> help
+  where
+    sendToServer :: MessagePack a => a -> IO ()
+    sendToServer = WS.sendBinaryData connection . MP.pack
+    {-putStrLn "Enter first number: "
     num1 <- read <$> getLine
     putStrLn "Enter second number: "
     num2 <- read <$> getLine
@@ -104,4 +136,6 @@ speaker connection = forever $ do
     let wsMsg = WSMessage tag (MP.pack msg)
     let packedMessage = MP.pack wsMsg
     WS.sendTextData connection packedMessage
-
+-}
+help :: IO ()
+help = putStrLn ""
